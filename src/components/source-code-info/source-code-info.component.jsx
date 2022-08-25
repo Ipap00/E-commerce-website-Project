2395bec5e48a7bee313afc3e47902f28774a9e05
Ipap00 <98342323+Ipap00@users.noreply.github.com>
2022-08-25 22:37:22 +0530

    Update source-code-info.component.jsx

--- src/components/source-code-info/source-code-info.component.jsx
+++ src/components/source-code-info/source-code-info.component.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 import './source-code-info.styles.css';
 
 const SourceCodeInfo = () => (
-    <h3 style={{ textAlign: "center" }}>
-        <a className='source-code-info' href={"https://github.com/ekinkaradag/sample-ecommerce"} target="_blank" rel="noopener noreferrer">
-            Source code can be found on GitHub
-        </a>
-    </h3>
 
 );
 
